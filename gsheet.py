9e7e049622d265f4be6eb678fe588358a86a67aa
Rostyslav Saenko <rostislav.saenko@gmail.com>
2020-01-14 00:46:20 +0200

    Remove obsolete comments, remove logging

--- gsheet.py
+++ gsheet.py
@@ -4,7 +4,7 @@ import db
 import pytz
 import pprint
 import gspread
-import logging
+#import logging
 import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -12,11 +12,8 @@ scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/aut
 creds = ServiceAccountCredentials.from_json_keyfile_name('env/python_tfbot_sheet.json', scope)
 client = gspread.authorize(creds)
 
-#date_time_now = datetime.datetime.now(pytz.timezone('Europe/Kiev')).strftime('%Y-%m-%d %H:%M:%S')
-
 def export_google_sheets() -> None:
   """Возвращает все расходы"""
-  logging.warning(f'export google sheets method called.')
   cursor = db.get_cursor()
   cursor.execute(
       "select e.id, e.amount, c.name, e.raw_text, e.created "
@@ -35,9 +32,6 @@ def export_google_sheets() -> None:
     })
 
   spreadsheet = client.open('TelegramFinanceBotReport')
-  #spreadsheet.del_worksheet(worksheet)
-  #spreadsheet.add_worksheet("All Database", 0, 6)
-  #ws = spreadsheet.worksheet("All Database")
   ws = spreadsheet.sheet1
   if ws.title != "All Database": ws.update_title("All Database") 
   ws.resize(1, 6)
@@ -45,28 +39,13 @@ def export_google_sheets() -> None:
   spreadsheet.values_update(
     'All Database!A1',
     params={
-        'valueInputOption': 'USER_ENTERED'
+      'valueInputOption': 'USER_ENTERED'
     },    
     body={
       'values': [[
         "##", "Номер строки", "Дата и время", "Сумма", "Категория", "Исходник"
       ]]
     })
-
-
-  #  for n in range(1,11):
-  #    ws.append_row([n, f'cell-A-row-{n}', f'cell-B-row-{n}', date_time_now], value_input_option='USER_ENTERED')
-
-  """
-  ws.append_row([
-    f'##',
-    f'Номер строки',
-    f'Дата и время',
-    f'Сумма',
-    f'Категория',
-    f'Исходник'
-  ], value_input_option='USER_ENTERED')
-  """
 
   n = 1
   for row in all_expenses:
@@ -80,5 +59,3 @@ def export_google_sheets() -> None:
     ],
     value_input_option='USER_ENTERED')
     n += 1
-
-  #logging.warning(f'the end of script.')
